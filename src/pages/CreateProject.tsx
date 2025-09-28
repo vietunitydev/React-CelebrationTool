f3cc50279aece2b6f483456df9527327263a7919
viet <vietunitydev2222@gmail.com>
2025-09-28 23:47:15 +0700

    - add admin key for writing data

--- src/pages/CreateProject.tsx
+++ src/pages/CreateProject.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { collection, addDoc } from 'firebase/firestore';
-import { db } from '../firebase';
+import { db, adminKey } from '../firebase';
 import { uploadImage, uploadAudio } from '../cloudinary';
 import { ProjectData } from "@/types/types.ts";
 
